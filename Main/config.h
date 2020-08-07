9203b1be05e1e810af013e8e0429071a8a50c82f
YuShun <2305808176@qq.com>
2020-08-07 21:46:45 +0800

    完善程序
    定义了引脚，正在调试超声波输出

---
+++ Main/config.h
@@ -0,0 +1,12 @@
+#ifndef __CONFIG_H_
+#define __CONFIG_H_
+
+#include "15w.h"
+
+#define LED_OUT P55
+#define SR_04_Gnd P54
+#define SR_04_Vcc P15
+#define SR_04_Echo P17
+#define SR_04_Trig P16
+
+#endif
--- Main/mian.c
+++ Main/mian.c
@@ -1,7 +1,66 @@
-#include "15w.h"
 #include "config.h"
+#include "INTRINS.H"
+
+// 提前声明含糊
+
+void Delay20us();       //20us延时函数，24MHz
+void Delay1000ms();     //1s延时函数，24MHz
 
 void main (void)
 {
-	
+    P5M0 = 0x30;
+    P5M1 = 0x00;
+    P1M0 = 0x20;
+    P1M1 = 0x00;
+    Delay20us();
+    SR_04_Vcc = 1;
+    SR_04_Gnd = 0;
+    Delay1000ms();
+    while (1)
+    {
+        SR_04_Echo = 0;
+        SR_04_Trig = 0;
+        Delay20us();
+        Delay20us();
+        SR_04_Trig = 1;
+        if(SR_04_Echo == 1)
+        {
+            LED_OUT = 0;
+        }
+        else
+        {
+            LED_OUT = 1;
+        }
+        Delay1000ms();
+    }
+    
+
+}
+
+void Delay20us()		//@24.000MHz
+{
+	unsigned char i;
+
+	_nop_();
+	_nop_();
+	i = 117;
+	while (--i);
+}
+
+void Delay1000ms()		//@24.000MHz
+{
+	unsigned char i, j, k;
+
+	_nop_();
+	_nop_();
+	i = 92;
+	j = 50;
+	k = 238;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
 }
